2b575f4423eed865612d04bbad2bfa87829f7bc1
Vinícius Thiengo <viniciusthiengo@gmail.com>
2018-08-18 19:31:09 -0300

    Update

--- app/src/main/java/thiengo/com/br/relgiomundialanimado/TrueTimeApplication.kt
+++ app/src/main/java/thiengo/com/br/relgiomundialanimado/TrueTimeApplication.kt
@@ -2,6 +2,7 @@ package thiengo.com.br.relgiomundialanimado
 
 import android.app.Application
 import android.content.Intent
+import android.os.SystemClock
 import android.support.v4.content.LocalBroadcastManager
 import com.instacart.library.truetime.TrueTimeRx
 import io.reactivex.schedulers.Schedulers
@@ -12,40 +13,48 @@ class TrueTimeApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        /*
-         * Sempre iniciaremos a busca por alguma data TrueTime, pois
-         * como o tick do aparelho é que será utilizado, ainda temos
-         * a possibilidade de ter uma data não atualizada se não houver
-         * uma nova conexão com a Internet. Mas lembrando que uma nova
-         * invocação a servidor NTP não remove da cache a TrueTime já
-         * salva.
-         * */
-        TrueTimeRx
-            .build()
-                .withSharedPreferences(this)
-                .initializeRx("time.apple.com")
-                .subscribeOn(Schedulers.io())
-                .subscribe(
-                        {
-                            /*
-                             * Tudo certo com a obtenção de uma data
-                             * TrueTime de servidor NTP, agora é preciso
-                             * atualizar o horário em tela, logo uma
-                             * "mensagem" broadcast é utilizada para
-                             * comunicar a atividade ClockActivity sobre
-                             * isso, a comunicação será por meio de
-                             * BroadcastApplication e LocalBroadcastManager.
-                             * */
-                            val intent = Intent( BroadcastApplication.FILTER )
+        /*Thread{
+            kotlin.run {
+                SystemClock.sleep(2000)*/
 
-                            LocalBroadcastManager
-                                    .getInstance( this@TrueTimeApplication )
-                                    .sendBroadcast( intent )
-                        },
-                        {
-                            /* Pilha de erro, caso ocorra. */
-                            /* it.printStackTrace(); */
-                        }
-                )
+                /*
+                 * Sempre iniciaremos a busca por alguma data TrueTime, pois
+                 * como o tick do aparelho é que será utilizado, ainda temos
+                 * a possibilidade de ter uma data não atualizada se não houver
+                 * uma nova conexão com a Internet. Mas lembrando que uma nova
+                 * invocação a servidor NTP não remove da cache a TrueTime já
+                 * salva.
+                 * */
+                TrueTimeRx
+                    .build()
+                        .withSharedPreferences(this)
+                        .initializeRx("time.apple.com")
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                                {
+                                    /*
+                                     * Tudo certo com a obtenção de uma data
+                                     * TrueTime de servidor NTP, agora é preciso
+                                     * atualizar o horário em tela, logo uma
+                                     * "mensagem" broadcast é utilizada para
+                                     * comunicar a atividade ClockActivity sobre
+                                     * isso, a comunicação será por meio de
+                                     * BroadcastApplication e LocalBroadcastManager.
+                                     * */
+                                    val intent = Intent( BroadcastApplication.FILTER )
+
+                                    LocalBroadcastManager
+                                            .getInstance( this@TrueTimeApplication )
+                                            .sendBroadcast( intent )
+                                },
+                                {
+                                    /* Pilha de erro, caso ocorra. */
+                                    /* it.printStackTrace(); */
+                                }
+                        )
+            /*}
+        }.start()*/
+
+
     }
 }
